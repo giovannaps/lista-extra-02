c37caafc952b73bf7cfd8b3b3364be2635583a2c
Giovanna Lima <giovannapslima.gpsl@gmail.com>
2025-05-11 18:14:27 -0300

    Add files via upload

---
+++ Gato.java
@@ -0,0 +1,8 @@
+public class Gato extends Animal{
+
+    @Override
+    public void Som(){
+        System.out.println("O gato faz: Miau");
+    };
+    
+}
---
+++ Zoologico.java
@@ -0,0 +1,13 @@
+
+
+
+public class Zoologico {
+    
+    Cachorro c1 = new Cachorro();
+    Gato g1 = new Gato();
+
+    public void emitirSons() {
+        c1.Som();
+        g1.Som();
+    }
+}
