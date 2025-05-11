4bf806d7a1a065a5342bf8ba3f712cb1a44721dd
Giovanna Lima <giovannapslima.gpsl@gmail.com>
2025-05-11 19:01:08 -0300

    Delete Empresa.java

--- Empresa.java
+++
@@ -1,9 +0,0 @@
-public class Empresa {
-
-    public void calcularFolhaDePagamento(Funcionario funcionario) {
-        System.out.println("Nome: " + funcionario.getNome());
-        System.out.println("Salário: " + funcionario.calcularSalario());
-        System.out.println("--------------------------------------------------");   
-    }   
-    
-}
